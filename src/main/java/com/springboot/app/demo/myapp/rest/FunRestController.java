a68515ac5998173f2ca38eaf525e0a74db35a872
gergefis <votanonkipos@gmail.com>
2024-06-28 19:37:25 +0300

    ADD more pages to website

--- src/main/java/com/springboot/app/demo/myapp/rest/FunRestController.java
+++ src/main/java/com/springboot/app/demo/myapp/rest/FunRestController.java
@@ -1,11 +1,28 @@
 package com.springboot.app.demo.myapp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+// BEGIN	inject properties for: coach,name and team.name
+	@Value("${coach.name}")
+	private String coachName;
+
+	@Value("${team.name}")
+	private String teamName;
+// END inject properties for: coach,name and team.name
+
+//	Expose new endpoint for "teaminfo"
+	@GetMapping("/teaminfo")
+	public String getTeamInfo(){
+		return "<b> Coach Name:</b> " + "<u>" + coachName + "</u>" + "<p><b> Team name: </b>" + "<u>" + teamName + "</u>" + "</p>";
+	}
+
+
 //	Expose "/" that return Hello World!"
 	@GetMapping("/")
 	public String sayHello() {
@@ -17,7 +34,7 @@ public class FunRestController {
 		return "Run hard for 5k!";
 	}
 //	Expose "/" that return fortune information
-	@GetMapping("fortune")
+	@GetMapping("/fortune")
 	public String getDailyFortune() {
 		return "Today is your lucky day.";
 	}
