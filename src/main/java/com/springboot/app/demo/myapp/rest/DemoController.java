cddb284abf3032b23d3b4d4901a4dc4caa05b4ef
gergefis <votanonkipos@gmail.com>
2024-07-03 18:20:09 +0300

    Add Qualifier annotation

--- src/main/java/com/springboot/app/demo/myapp/rest/DemoController.java
+++ src/main/java/com/springboot/app/demo/myapp/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.springboot.app.demo.myapp.rest;
 
 import com.springboot.app.demo.myapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController{
 	private Coach myCoach;
 
+	// setter method
 	@Autowired // Tells Spring to inject a dependency,
-				// if we have only 1 Constructor, then, this annotation on constructor is optional
-	public DemoController(Coach theCoach){ // Constructor
+	// if we have only 1 Constructor, then, this annotation is optional
+	public void setCoach(@Qualifier("tennisCoach") Coach theCoach){
 		myCoach = theCoach;
 	}
 	@GetMapping("/dailyworkout")
 	public String getDailyWorkout(){
 		return myCoach.getDailyWorkout();
 	}
-
 }
